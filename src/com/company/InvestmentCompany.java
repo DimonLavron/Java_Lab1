c2f1fc2d3744bab8c68f7aa425be7d0bd0be4a90
Dima <dima.lavrishin@gmail.com>
2019-02-12 16:26:33 +0200

    Clean code, constructors changed, getters and setters for all variables, usage of resetValues method added

--- src/com/company/InvestmentCompany.java
+++ src/com/company/InvestmentCompany.java
@@ -1,7 +1,6 @@
 package com.company;
 
-public class InvestmentCompany
-{
+public class InvestmentCompany {
     private String address;
     private int ownedMoney;
     private int investedMoney;
@@ -14,90 +13,102 @@ public class InvestmentCompany
     protected int numberOfClients;
     protected double percentages;
 
-    public InvestmentCompany()
-    {
-        numberOfCompanies++;
+    public InvestmentCompany() {
+        this("", 0, 0, "", "", 0, 0.0);
     }
 
-    public InvestmentCompany(String address, int ownedMoney, int investedMoney, String name)
-    {
-        this();
+    public InvestmentCompany(String address, int ownedMoney, int investedMoney, String name) {
+        this(address, ownedMoney, investedMoney, name, "", 0, 0.0);
+    }
+
+    public InvestmentCompany(String address, int ownedMoney, int investedMoney, String name, String director, int numberOfClients, double percentages) {
         this.address = address;
         this.ownedMoney = ownedMoney;
         this.investedMoney = investedMoney;
         this.name = name;
-    }
-
-    public InvestmentCompany(String address, int ownedMoney, int investedMoney, String name, String director, int numberOfClients, double percentages)
-    {
-        this(address, ownedMoney, investedMoney, name);
         this.director = director;
         this.numberOfClients = numberOfClients;
         this.percentages = percentages;
+        numberOfCompanies++;
     }
 
-    public void setAddress(String address)
-    {
-        this.address = address;
-    }
-    public String getAddress()
-    {
+    public String getAddress() {
         return address;
     }
 
-    public void setOwnedMoney(int ownedMoney)
-    {
-        this.ownedMoney = ownedMoney;
+    public void setAddress(String address) {
+        this.address = address;
     }
-    public int getOwnedMoney()
-    {
+
+    public int getOwnedMoney() {
         return ownedMoney;
     }
 
-    public void setInvestedMoney(int investedMoney)
-    {
-        this.investedMoney = investedMoney;
+    public void setOwnedMoney(int ownedMoney) {
+        this.ownedMoney = ownedMoney;
     }
-    public int getInvestedMoney()
-    {
+
+    public int getInvestedMoney() {
         return investedMoney;
     }
 
-    public void setName(String name)
-    {
-        this.name = name;
+    public void setInvestedMoney(int investedMoney) {
+        this.investedMoney = investedMoney;
     }
-    public String getName()
-    {
+
+    public String getName() {
         return name;
     }
 
-    public void setDirector(String director)
-    {
-        this.director = director;
+    public void setName(String name) {
+        this.name = name;
     }
-    public String getDirector()
-    {
+
+    public String getDirector() {
         return director;
     }
 
-    public String toString()
-    {
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public static int getNumberOfCompanies() {
+        return numberOfCompanies;
+    }
+
+    public static void setNumberOfCompanies(int numberOfCompanies) {
+        InvestmentCompany.numberOfCompanies = numberOfCompanies;
+    }
+
+    public int getNumberOfClients() {
+        return numberOfClients;
+    }
+
+    public void setNumberOfClients(int numberOfClients) {
+        this.numberOfClients = numberOfClients;
+    }
+
+    public double getPercentages() {
+        return percentages;
+    }
+
+    public void setPercentages(double percentages) {
+        this.percentages = percentages;
+    }
+
+    public String toString() {
         return "Address: " + address + "; Owned Money: " + ownedMoney + "; Invested Money: " + investedMoney + "; Name: " + name + "; Director: " + director + "; Number of clients: " + numberOfClients + "; Persentages: " + percentages + ";";
     }
 
-    public static void printStaticNumberOfCompanies()
-    {
+    public static void printStaticNumberOfCompanies() {
         System.out.println("Number of companies: " + numberOfCompanies);
     }
 
-    public void printNumberOfCompanies()
-    {
+    public void printNumberOfCompanies() {
         System.out.println("Number of companies: " + numberOfCompanies);
     }
 
-    public void ResetValues(String address, int ownedMoney, int investedMoney, String name, String director, int numberOfClients, double percentages)
-    {
+    public void resetValues(String address, int ownedMoney, int investedMoney, String name, String director, int numberOfClients, double percentages) {
         this.address = address;
         this.ownedMoney = ownedMoney;
         this.investedMoney = investedMoney;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	InvestmentCompany company1 = new InvestmentCompany();
-	InvestmentCompany company2 = new InvestmentCompany("Address", 100, 500, "Polyglot");
-	InvestmentCompany company3 = new InvestmentCompany("Address", 60, 428, "Parus", "Petro", 66, 12.5);
-	System.out.println(company1);
-	System.out.println(company2);
-	System.out.println(company3);
-	InvestmentCompany.printStaticNumberOfCompanies();
-	company1.printNumberOfCompanies();
+		InvestmentCompany company1 = new InvestmentCompany();
+		InvestmentCompany company2 = new InvestmentCompany("Address", 100, 500, "Polyglot");
+		InvestmentCompany company3 = new InvestmentCompany("Address", 60, 428, "Parus", "Petro", 66, 12.5);
+		System.out.println(company1);
+		System.out.println(company2);
+		System.out.println(company3);
+		InvestmentCompany.printStaticNumberOfCompanies();
+		company1.printNumberOfCompanies();
+		company3.resetValues("New address", 63, 511, "Parus", "Ivan", 68, 12.5);
+		System.out.println(company3);
     }
 }
